bd777f06fc4621ebe72d2cce8134db68eb8e44f1
Kacy236 <ndubuisik216@gmail.com>
2025-08-11 20:48:15 +0100

    finished setting up TRPC and added categories router

---
+++ src/modules/categories/server/procedures.ts
@@ -0,0 +1,28 @@
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { Categories } from '@/payload-types';
+
+export const categoriesRouter = createTRPCRouter({
+    getMany: baseProcedure.query(async ({ ctx }) => {
+        const data = await ctx.db.find({
+            collection: 'categories',
+            depth: 1, // Populate subcategories, subcategories.[0] will be a type of "Category"
+            pagination: false,
+            where: {
+              parent: {
+                exists: false,
+              },
+            },
+            sort: "name"
+          });
+
+          const formattedData: CustomCategory[] = data.docs.map((doc) => ({
+            ...doc,
+            subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+                // Because of "depth: 1" we are confident "doc" will be a type of Category
+                ...(doc as Categories),
+                subcategories: undefined,
+            }))
+        }))
+        return formattedData;
+    }),
+});
