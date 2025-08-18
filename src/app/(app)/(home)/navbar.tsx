b94961470a4aaf94063bae727a33c303fe443abd
Kacy236 <ndubuisik216@gmail.com>
2025-08-18 01:11:43 +0100

    redirecting to dashboard after signup or login

--- src/app/(app)/(home)/navbar.tsx
+++ src/app/(app)/(home)/navbar.tsx
@@ -9,6 +9,8 @@ import { useState } from "react";
 import { MenuIcon } from "lucide-react";
 import Link from "../../../../node_modules/next/link";
 import { usePathname } from "../../../../node_modules/next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 const poppins = Poppins({
     subsets: ["latin"],
@@ -55,6 +57,9 @@ export const Navbar = () => {
     const pathname = usePathname();
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+    const trpc = useTRPC();
+    const session = useQuery(trpc.auth.session.queryOptions());
+
     return (
         <nav className="h-20 flex border-b justify-between font-medium bg-white">
             <Link href="/" className="pl-6 flex items-center">
@@ -81,6 +86,18 @@ export const Navbar = () => {
                 ))}
             </div>
 
+            {session.data?.user ? (
+                <div className="hidden lg:flex">
+                <Button
+                  asChild
+                  className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors text-lg"
+                >
+                    <Link href="/admin">
+                      Dashboard
+                    </Link>
+                </Button>
+                </div>
+            ) : (
             <div className="hidden lg:flex">
                 <Button
                   asChild
@@ -100,6 +117,7 @@ export const Navbar = () => {
                     </Link>
                 </Button>
             </div>
+            )}
 
             <div className="flex lg:hidden items-center justify-center">
                 <Button
