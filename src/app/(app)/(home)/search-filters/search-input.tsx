a22122f47cc036113b9efb41be5c24ad47cb19ea
Kacy236 <ndubuisik216@gmail.com>
2025-08-18 01:53:56 +0100

    use authenticated states

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { SearchIcon, ListFilterIcon } from "lucide-react"
+import { SearchIcon, ListFilterIcon, BookmarkCheckIcon } from "lucide-react"
 
 import { Input } from "@/components/ui/input"
 import { CategoriesSidebar } from "./categories-sidebar"
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
+import { useTRPC } from "@/trpc/client"
+import { useQuery } from "@tanstack/react-query";
+import Link from "../../../../../node_modules/next/link";
 
 interface Props {
     disabled?: boolean;
@@ -16,6 +19,10 @@ export const SearchInput = ({
 }: Props) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+    const trpc = useTRPC()
+
+    const session = useQuery(trpc.auth.session.queryOptions());
+
     return (
         <div className="flex items-center gap-2 w-full">
             <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen}/>
@@ -23,7 +30,6 @@ export const SearchInput = ({
                 <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
                 <Input className="pl-8" placeholder="Search Products" disabled={disabled}/>
             </div>
-            {/* TODO: Add categories view all button */}
             <Button
               variant="elevated"
               className="size-12 shrink-0 flex lg:hidden"
@@ -31,7 +37,17 @@ export const SearchInput = ({
             >
                 <ListFilterIcon />
             </Button>
-            {/* TODO: Add Library button */}
+            {session.data?.user && (
+                <Button
+                  asChild
+                  variant="elevated"
+                >
+                    <Link href="/library">
+                        <BookmarkCheckIcon />
+                        Library
+                    </Link>
+                </Button>
+            )}
         </div>
     )
 }
