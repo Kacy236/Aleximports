672e0b1b6ce6d5145dcc9a4da19ecce98f7623aa
Kacy236 <ndubuisik216@gmail.com>
2025-08-26 12:03:35 +0100

    created a loading skeleton for products ui

--- src/modules/products/ui/components/product-card.tsx
+++ src/modules/products/ui/components/product-card.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { StarIcon } from "lucide-react";
 
 interface ProductCardProps {
     id: string;
@@ -23,8 +24,8 @@ export const ProductCard = ({
     price,
 }: ProductCardProps) => {
     return (
-    <Link href="/">
-      <div className="border rounded-md bg-white overflow-hidden h-full flex flex-col">
+    <Link href={`/products/${id}`}>
+      <div className="hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-shadow border rounded-md bg-white overflow-hidden h-full flex flex-col">
           <div className="relative aspect-square">
               <Image
                 alt={name}
@@ -48,8 +49,33 @@ export const ProductCard = ({
                   )}
                   <p className="text-sm underline font-medium">{authorUsername}</p>
               </div>
+              {reviewCount > 0 && (
+                  <div className="flex items-center gap-1">
+                      <StarIcon className="size-3.5 fill-black" />
+                      <p className="text-sm font-medium">
+                          {reviewRating} ({reviewCount})
+                      </p>
+                  </div>
+              )}
+          </div>
+          <div className="p-4">
+              <div className="relative px-2 py-1 border bg-pink-400 w-fit">
+                  <p className="text-sm font-medium">
+                      {new Intl.NumberFormat("en-US", {
+                          style: "currency",
+                          currency: "USD",
+                          maximumFractionDigits: 0,
+                      }). format(Number(price))}
+                  </p>
+              </div>
           </div>
       </div>
     </Link>
    )
+}
+
+export const ProductCardSkeleton = () => {
+    return (
+        <div className="w-full aspect-3/4 bg-neutral-200 rounded-lg animate-pulse"/>
+    )
 }
