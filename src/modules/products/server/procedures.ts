78b3b32c29606a1e2ca5e064a3ee662217006f2e
Kacy236 <ndubuisik216@gmail.com>
2025-08-26 12:26:06 +0100

    added infinte load

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -4,11 +4,14 @@ import type { Sort, Where } from "payload";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { Categories, Media } from "@/payload-types";
 import { sortValues } from "../search-params";
+import { DEFAULT_LIMIT } from "@/constants";
 
 export const productsRouter = createTRPCRouter({
     getMany: baseProcedure
     .input(
       z.object({
+        cursor: z.number().default(1),
+        limit: z.number().default(DEFAULT_LIMIT),
         category: z.string().nullable().optional(),
         minPrice: z.string().nullable().optional(),
         maxPrice: z.string().nullable().optional(),
@@ -95,6 +98,8 @@ export const productsRouter = createTRPCRouter({
             depth: 1, // Populate "category" & "image"
             where,
             sort,
+            page: input.cursor,
+            limit: input.limit,
           });
 
         return {
