9d798fc3ae9110d6f91322a586dba621fa3ec7e0
Kacy236 <ndubuisik216@gmail.com>
2025-08-31 00:38:48 +0100

    working on cart and checkout page

--- src/modules/products/ui/components/cart-button.tsx
+++ src/modules/products/ui/components/cart-button.tsx
@@ -1,3 +1,25 @@
+import { Button } from "@/components/ui/button";
+
+import { useCart } from "@/modules/checkout/hooks/use-cart";
+
 interface Props {
-    
-}
+    tenantSlug: string;
+    productId: string;
+};
+
+export const CartButton = ({ tenantSlug, productId }: Props) => {
+    const cart = useCart(tenantSlug);
+
+    return (
+        <Button
+          variant="elevated"
+          className="flex-1 bg-pink-400"
+          onClick={() => cart.toggleProduct(productId)}
+        >
+          {cart.isProductInCart(productId)
+            ? "Remove from cart"
+            : "Add to cart"
+          }
+        </Button>
+    );
+};
