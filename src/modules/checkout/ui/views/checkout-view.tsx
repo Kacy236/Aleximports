3a4df0fdf08bd5547e94c678306767f3939a6604
Kacy236 <ndubuisik216@gmail.com>
2025-09-06 10:56:31 +0100

    checkout sidebar

--- src/modules/checkout/ui/views/checkout-view.tsx
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -2,7 +2,7 @@
 
 import { toast } from "sonner";
 import { useTRPC } from "@/trpc/client";
-import { InboxIcon } from "lucide-react";
+import { InboxIcon, LoaderIcon } from "lucide-react";
 
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
@@ -20,7 +20,7 @@ export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
     const { productIds, removeProduct, clearAllCarts } = useCart(tenantSlug);
 
     const trpc = useTRPC();
-    const { data, error } = useQuery(trpc.checkout.getProducts.queryOptions({
+    const { data, error, isLoading } = useQuery(trpc.checkout.getProducts.queryOptions({
         ids: productIds,
     }));
 
@@ -29,13 +29,25 @@ export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
             clearAllCarts();
             toast.warning("Invalid products found, cart cleared");
         }
-    }, [error, clearAllCarts])
+    }, [error, clearAllCarts]);
+
+if (isLoading) {
+    return (
+        <div className="lg:pt-16 pt-4 px-4 lg:px-12">
+        <div className="border border-black border-dashed flex items-center justify-center p-8 flex-col gap-y-4 bg-white w-full rounded-lg">
+           <LoaderIcon className="text-muted-foreground animate-spin" />     
+        </div>
+        </div>
+    )
+}
 
     if (data?.totalDocs === 0) {
         return (
+            <div className="lg:pt-16 pt-4 px-4 lg:px-12">
             <div className="border border-black border-dashed flex items-center justify-center p-8 flex-col gap-y-4 bg-white w-full rounded-lg">
                 <InboxIcon />
                 <p className="text-base font-medium">No Products found</p>
+            </div>
             </div>
         );
     }
@@ -43,7 +55,7 @@ export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
     return (
         <div className="lg:pt-16 pt-4 px-4 lg:px-12">
             <div className="grid grid-cols-1 lg:grid-cols-7 gap-4 lg:gap-16">
-
+ 
                 <div className="lg:col-span-4">
                     <div className="border rounded-md overflow-hidden bg-white">
                         {data?.docs.map((product, index) => (
