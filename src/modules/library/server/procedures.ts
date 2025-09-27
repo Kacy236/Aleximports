4c6197f53d5b91813d3ac6f60634a706c68493ce
Kacy236 <ndubuisik216@gmail.com>
2025-09-27 04:13:53 +0100

    create getOne library procedure

--- src/modules/library/server/procedures.ts
+++ src/modules/library/server/procedures.ts
@@ -3,9 +3,62 @@ import z from "zod";
 import { protectedProcedure, createTRPCRouter } from "@/trpc/init";
 import { Media, Tenants } from "@/payload-types";
 import { DEFAULT_LIMIT } from "@/constants";
+import { TRPCError } from "@trpc/server";
 
 export const libraryRouter = createTRPCRouter({
-  getMany: protectedProcedure
+  getOne: protectedProcedure
+    .input(
+      z.object({
+        productId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const ordersData = await ctx.db.find({
+        collection: "orders",
+        limit: 1,
+        pagination: false,
+        where: {
+          and: [
+            {
+              products: { equals: input.productId },
+            },
+            {
+              user: { equals: ctx.session.user.id },
+            },
+            {
+              status: { equals: "success" }, // ✅ only successful orders
+            },
+          ],
+        },
+      });
+
+      // ✅ flatten product arrays from all orders
+      const order = ordersData.docs[0];
+
+      if (!order) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Order not found",
+        });
+      }
+      
+
+      const product = await ctx.db.findByID({
+        collection: "products",
+          id: input.productId,
+      });
+
+      if (!product) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Product not found",
+        });
+      }
+
+      return product
+    }),
+
+    getMany: protectedProcedure
     .input(
       z.object({
         cursor: z.number().default(1),
@@ -55,4 +108,5 @@ export const libraryRouter = createTRPCRouter({
         })),
       };
     }),
+
 });
