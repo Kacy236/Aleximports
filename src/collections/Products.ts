40b37481cd9000652fc54eadf5821e83c4d7d60b
Kacy236 <ndubuisik216@gmail.com>
2025-10-25 15:27:22 +0100

    allow tenants to hide their products from the marketplace

--- src/collections/Products.ts
+++ src/collections/Products.ts
@@ -111,6 +111,15 @@ export const Products: CollectionConfig = {
       },
     },
     {
+        name: "isPrivate",
+        label: "Private",
+        defaultValue: false,
+        type: "checkbox",
+        admin: {
+            description: "If checked, this product will not be shown on the public storefront"
+        },
+    },
+    {
         name: "isArchived",
         label: "Archive",
         defaultValue: false,
--- src/payload-types.ts
+++ src/payload-types.ts
@@ -277,6 +277,10 @@ export interface Product {
    */
   content?: string | null;
   /**
+   * If checked, this product will not be shown on the public storefront
+   */
+  isPrivate?: boolean | null;
+  /**
    * If checked, this product will be archived
    */
   isArchived?: boolean | null;
@@ -497,6 +501,7 @@ export interface ProductsSelect<T extends boolean = true> {
   image?: T;
   refundPolicy?: T;
   content?: T;
+  isPrivate?: T;
   isArchived?: T;
   updatedAt?: T;
   createdAt?: T;
