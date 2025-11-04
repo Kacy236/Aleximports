387cb9b914afd879b86837c54416c3b17ca95a75
Kacy236 <ndubuisik216@gmail.com>
2025-11-04 11:58:59 +0100

    deployment

--- src/modules/auth/utils.ts
+++ src/modules/auth/utils.ts
@@ -1,23 +1,21 @@
-import { cookies as getCookies } from "next/headers";
+import { cookies } from "next/headers";
 
 interface Props {
-    prefix: string;
-    value: string;
+  prefix: string;
+  value: string;
 }
 
-export const generateAuthCookie = async ({
-    prefix,
-    value,
-}: Props) => {
-    const cookies = await getCookies();
+export const generateAuthCookie = ({ prefix, value }: Props) => {
+  const cookieStore = cookies();
 
-          cookies.set({
-              name: `${prefix}-token`,
-              value: value,
-              httpOnly: true,
-              path: "/",
-              sameSite: "none",
-              domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
-              secure: process.env.NODE_ENV === "production",
-          });
+  cookieStore.set({
+    name: `${prefix}-token`,
+    value,
+    httpOnly: true,
+    path: "/",
+    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+    secure: process.env.NODE_ENV === "production",
+    domain: process.env.NEXT_PUBLIC_ROOT_DOMAIN?.replace(/^https?:\/\//, ""),
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+  });
 };
