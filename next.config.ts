838256717420af52e782851cbf9bec125fd832b9
Ndubuisi Kelechi <ndubuisik216@gmail.com>
2025-12-13 17:51:00 +0100

    new changes

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ replaces --no-lint
+  // 🔴 THIS is the key line
+  experimental: {
+    forceSwcTransforms: true,
   },
 
   webpack: (config) => config,
